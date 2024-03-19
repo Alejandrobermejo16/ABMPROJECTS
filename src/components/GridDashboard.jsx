6be95fb6e737fa4cee5f418926c36125b0b8581b
Alejandrobermejo16 <alejandrobermejomendez170712@gmail.com>
2024-03-19 16:41:06 +0100

    añadido boton eliminar y checkbox

--- src/components/GridDashboard.jsx
+++ src/components/GridDashboard.jsx
@@ -9,7 +9,7 @@ class GridDashboard extends Component {
 
     constructor() {
         super();
-        this.state = { openModal: false, openModal2: false, tareaYaEscrita: false, inputValue: '' };
+        this.state = { openModal: false, openModal2: false, tareaYaEscrita: false, inputValue: '', disabledCheckbox: true };
     }
 
     componentDidUpdate(prevProps) {
@@ -58,9 +58,16 @@ class GridDashboard extends Component {
 
     }
 
+    //cambia el estado de visibilidad del checkbox cada vez que se le llama
+    statusCheckbox = () => {
+        const { disabledCheckbox } = this.state;
+        alert('se ha cambiado el checkbox');
+        this.setState({ disabledCheckbox: !disabledCheckbox })
+    }
+
     render() {
         const { dataRandom, fetchColumnAleatorioAction, addTaskAction, listProjects, listTasks, loading } = this.props;
-        const { openModal, openModal2, tareaYaEscrita, inputValue } = this.state;
+        const { openModal, openModal2, tareaYaEscrita, inputValue, disabledCheckbox } = this.state;
         //creo un array vacio donde guardaremos el nombre de los proyectos o tareas 
         //dentro a ese array le metemos una lista que tendra como key , las claves del objeto pero solo accederemos
         //a name
@@ -96,19 +103,32 @@ class GridDashboard extends Component {
                     </Col>
                     <Col>
                         <h3>Tareas realizadas</h3>
-                        {/* se obtiene el nombre del proyecto y se busca que la tarea coincida con cada proyecto para mostrarla dentro de cada proyecto */}
-                        { listProjects.listProjects && listProjects.listProjects.map((project, index) => (
+                         {/* se obtiene el nombre del proyecto y se busca que la tarea coincida con cada proyecto para mostrarla dentro de cada proyecto */}
+                        {listProjects.listProjects && listProjects.listProjects.map((project, index) => (
                             <div key={index}>
                                 <h4>{project.name}</h4>
                                 {listTasks.listTasks && listTasks.listTasks
                                     .filter(task => task.projectId === project.id)
                                     .map((task, taskIndex) => (
-                                        <p style={{color: 'yellow'}} key={taskIndex}>{task.content}</p>
+                                        <div key={taskIndex} style={{ display: 'flex', alignItems: 'center' }}>
+                                            {!this.state.disabledCheckbox && 
+                                            <input 
+                                             type="checkbox" 
+                                             onChange={this.statusCheckbox}
+                                             />}
+                                            <p style={{ color: 'yellow', marginLeft: '5px' }} key={taskIndex}>{task.content}</p>
+                                        </div>
                                     ))}
                             </div>
                         ))}
 
-                        <Button variant="primary" onClick={this.openModalAddTask}>Añadir Tarea</Button>
+
+
+                        <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '10px' }}>
+                            <Button size="sm" variant="primary" onClick={this.openModalAddTask}>Añadir Tarea</Button>
+                            <Button size="sm" variant="primary" onClick={this.statusCheckbox}>Eliminar Tarea</Button>
+                        </div>
+
                         <p>Contenido de la columna 2</p>
                         <p>Contenido de la columna 2</p>
                         <p>Contenido de la columna 2</p>
@@ -132,7 +152,7 @@ class GridDashboard extends Component {
                     <Col>
                         <h3>Palabra aleatoria</h3>
                         <Button variant="dark" onClick={fetchColumnAleatorioAction}>Aleatorio</Button>
-                        {dataRandom && <h1 style={{color: 'yellow'}}>{dataRandom.dataRandom}</h1>}
+                        {dataRandom && <h1 style={{ color: 'yellow' }}>{dataRandom.dataRandom}</h1>}
                         <p>Contenido de la columna 5</p>
                         <p>Contenido de la columna 5</p>
                         <p>Contenido de la columna 5</p>
