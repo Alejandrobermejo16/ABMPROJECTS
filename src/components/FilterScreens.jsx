1d7805a1cfaeb4ba8ad09cef28ced47e724b6388
Alejandrobermejo16 <alejandrobermejomendez170712@gmail.com>
2024-03-25 17:36:35 +0100

    cambios en el texto y filtros

--- src/components/FilterScreens.jsx
+++ src/components/FilterScreens.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Nav, Accordion, Card, Button } from 'react-bootstrap';
-import { ExclamationTriangleFill  } from 'react-bootstrap-icons';
+import { ExclamationTriangleFill } from 'react-bootstrap-icons';
 
 import '../styles/InitialFilter.css';
 // Importa los componentes de pantalla aquí
@@ -54,27 +54,26 @@ class FilterScreens extends React.Component {
                 </div>
                 {noData ? (
                     <div style={{ marginTop: '20px' }}>
-                        <Accordion defaultActiveKey="0">
+                        <Accordion defaultActiveKey="0" >
                             <Accordion.Item eventKey="0">
-                                <Accordion.Header>Accordion Item #1</Accordion.Header>
+                                <Accordion.Header>ALEJANDRO BERMEJO MENDEZ</Accordion.Header>
                                 <Accordion.Body>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                                    aliquip ex ea commodo consequat. Duis aute irure dolor in
-                                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                                    culpa qui officia deserunt mollit anim id est laborum.
+                                    <p>
+                                        Hola, soy Alejandro, un programador frontend con experiencia en el desarrollo de aplicaciones utilizando React, tanto con componentes de clase como sin el uso de hooks. Mi enfoque se centra en garantizar la estabilidad y eficiencia del código, priorizando una experiencia del usuario excepcional desde el primer clic.
+                                    </p>
+
+                                    <p>
+                                        Además de mis habilidades técnicas, tengo una fuerte ética de trabajo y un compromiso continuo con el crecimiento profesional. Siempre estoy dispuesto a aprender nuevas tecnologías y metodologías, y estoy constantemente buscando desafíos que me permitan expandir mis habilidades y conocimientos.
+                                    </p>
                                 </Accordion.Body>
                             </Accordion.Item>
                         </Accordion>
                         <div style={{ display: 'flex', flexDirection: 'row' }}>
                             <div style={{ marginRight: '20px' }}>
                                 <div style={{ display: 'inline-block', padding: '5px', border: '2px solid yellow', borderRadius: '5px' }}>
-                                    <Card style={{ width: '10rem', height: '20%', border: 'none' }}>
+                                    <Card style={{ width: '10rem', height: '40%', border: 'none' }}>
                                         <Card.Img variant="top" src={ImagenAlejandro} alt='Imagen del desarrollador de la web' />
                                         <Card.Body >
-                                            <Card.Title>Alejandro Bermejo Mendez</Card.Title>
                                             <Card.Text>
                                                 Desarrollador Front-end en continuo aprendizaje, con ganas de aportar y aprender.
                                             </Card.Text>
@@ -85,7 +84,7 @@ class FilterScreens extends React.Component {
                             </div>
 
                             {!loadingSkills ? (
-                                <div style={{paddingTop: '60px', paddingLeft: '300px'}}> 
+                                <div style={{ paddingTop: '60px', paddingLeft: '300px' }}>
                                     <ExclamationTriangleFill /> Para poder ver todas las habilidades laborales pulsa el botón de Skills Profesionales
                                 </div>
                             ) : ''}
