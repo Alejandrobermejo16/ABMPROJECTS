d2d95f9bc31f5f06c33b7e8c124fe7d8161d6b31
Alejandrobermejo16 <alejandrobermejomendez170712@gmail.com>
2024-04-05 14:20:51 +0200

    cambios en modal

--- src/screens/ReservasHipica.jsx
+++ src/screens/ReservasHipica.jsx
@@ -4,12 +4,17 @@ import GaleriaImagenes from '../helpers/GaleriaImagenes';
 import Clock from '../helpers/Clock';
 import { GALERIARESERVASHIPICA } from '../Constants';
 import { CalendarDateFill } from 'react-bootstrap-icons';
-import { Button, Modal } from 'react-bootstrap';
+import { Button, Modal, InputGroup, Form } from 'react-bootstrap';
+import Datetime from 'react-datetime'; // Importa Datetime
+import 'react-datetime/css/react-datetime.css'; // Importa los estilos
 
 class ReservasHipica extends React.Component {
     constructor() {
         super();
-        this.state = { openModal: false };
+        this.state = {
+            openModal: false,
+            selectedDate: new Date() // Inicializa la fecha seleccionada
+        };
     }
 
     openModalReserve = () => {
@@ -20,8 +25,12 @@ class ReservasHipica extends React.Component {
         this.setState({ openModal: false });
     }
 
+    handleDateChange = (date) => {
+        this.setState({ selectedDate: date });
+    }
+
     render() {
-        const { openModal } = this.state;
+        const { openModal, selectedDate } = this.state;
 
         return (
             <div className='ReservasHipica' style={{ height: '100vh', width: '100vw' }}>
@@ -38,12 +47,44 @@ class ReservasHipica extends React.Component {
                     </Button>
                 </div>
 
-                <Modal show={openModal} fullscreen centered onHide={this.closeModal}>
+                <Modal show={openModal} size="xl" centered onHide={this.closeModal}>
                     <Modal.Body>
                         <div className="flex items-center md:items-start gap-2">
+                            <InputGroup className="mb-3">
+                                <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+                                <Form.Control
+                                    placeholder="Nombre"
+                                    aria-label="Nombre"
+                                    aria-describedby="basic-addon1"
+                                />
+                            </InputGroup>
+                            <InputGroup className="mb-3">
+                                <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+                                <Form.Control
+                                    placeholder="Apellidos"
+                                    aria-label="Apellidos"
+                                    aria-describedby="basic-addon1"
+                                />
+                            </InputGroup>
                             <h3 className="text-body-4 md:text-body-2 font-semibold text-metal-900">
                                 Introduce la fecha de la reserva
                             </h3>
+                            <div style={{ display: 'flex', alignItems: 'center' }}>
+                                <CalendarDateFill color="black" size={40} style={{ paddingRight: '10px' }} />
+                                <Datetime
+                                    value={selectedDate}
+                                    onChange={this.handleDateChange}
+                                />
+                            </div>
+                            <InputGroup className="mb-3" style={{ paddingTop: '10px' }}>
+                                <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+                                <Form.Control
+                                    placeholder="direccion de correo"
+                                    aria-label="direccion de correo"
+                                    aria-describedby="direccion de correo"
+                                />
+                                <InputGroup.Text id="direccion de correo">@example.com</InputGroup.Text>
+                            </InputGroup>
                         </div>
                     </Modal.Body>
                     <Modal.Footer>
