47758bccf12a709e403d8884e2350091e904c565
Alejandrobermejo16 <alejandrobermejomendez170712@gmail.com>
2024-05-17 16:02:53 +0200

    fix

--- src/helpers/Clock.jsx
+++ src/helpers/Clock.jsx
@@ -5,7 +5,7 @@ class Clock extends React.Component {
         super();
         this.state = { reloj: new Date() };
     }
-    
+
     componentDidMount() {
         // Actualizar la hora cada segundo
         this.intervalID = setInterval(
@@ -13,12 +13,12 @@ class Clock extends React.Component {
             1000
         );
     }
-    
+
     componentWillUnmount() {
         // Limpiar el intervalo cuando el componente se desmonta
         clearInterval(this.intervalID);
     }
-    
+
     tick() {
         // Actualizar el estado para reflejar la hora actual
         this.setState({
@@ -28,20 +28,20 @@ class Clock extends React.Component {
 
     formatearFecha(formato) {
         const { reloj } = this.state;
-    
+
         // Extraer los componentes de la fecha
         const horas = String(reloj.getHours()).padStart(2, '0');
         const minutos = String(reloj.getMinutes()).padStart(2, '0');
         const segundos = String(reloj.getSeconds()).padStart(2, '0');
         const dia = String(reloj.getDate()).padStart(2, '0'); // Obtener el día del mes
         const año = reloj.getFullYear(); // Obtener el año
-    
+
         // Array para los nombres de los meses
         const nombresMeses = [
             "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
             "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
         ];
-    
+
         // Formatear la fecha según el formato especificado
         switch (formato) {
             case 'hora':
@@ -53,19 +53,19 @@ class Clock extends React.Component {
                 return 'Formato no válido';
         }
     }
-    
-    
+
+
     render() {
         const { formato } = this.props;
-    
+
         return (
             <div>
-                <h7 style={{ paddingLeft: '1350px' }}>
+                <span style={{ paddingLeft: '1350px' }}>
                     {formato === 'hora' && this.formatearFecha('hora')}
                     {formato === 'fecha' && this.formatearFecha('fecha')}
-                    {formato === 'horayfecha' ? `${this.formatearFecha('hora')} - ${this.formatearFecha('fecha')}` : ''}
+                    {formato === 'horayfecha' && `${this.formatearFecha('hora')} - ${this.formatearFecha('fecha')}`}
+                </span>
 
-                </h7>
             </div>
         );
     }
