1f22536f4949d79837de08b48cdbc99c668853a9
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-06-21 14:31:21 +0200

    prueba

--- src/components/AddUserForm.jsx
+++ src/components/AddUserForm.jsx
@@ -9,23 +9,14 @@ const AddUserForm = () => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-
-// Middleware para permitir solicitudes CORS desde un origen específico
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://abmprojects-7kay.vercel.app/api/users');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
-
+  
     try {
       const response = await axios.post('https://backendabmprojects.vercel.app/api/users', {
         name,
         email,
         password
       });
-
+  
       if (response.status === 201) {
         setMessage('Usuario añadido correctamente');
       } else {
@@ -36,7 +27,7 @@ app.use((req, res, next) => {
       setMessage('Error al enviar el formulario');
     }
   };
-
+  
   return (
     <form onSubmit={handleSubmit}>
       <input
