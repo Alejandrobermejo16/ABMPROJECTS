c65334e203948c77bda4b1f80d7c9acb75c81d73
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-06-26 09:58:40 +0200

    adelanto con modal

--- src/screens/LoguinUserScreen.jsx
+++ src/screens/LoguinUserScreen.jsx
@@ -1,27 +1,105 @@
-import React, { useState } from 'react';
-import AddUserForm from '../components/AddUserForm';
-import '../styles/abmLoggingScreen.css';
+import React, { useState } from "react";
+import AddUserForm from "../components/AddUserForm";
+import "../styles/abmLoggingScreen.css";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import axios from 'axios';
 
 const LoginUserScreen = () => {
-    const [loadScreen, setLoadScreen] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
 
-    const handleLoginClick = () => {
-        // Aquí iría la lógica para hacer la llamada a la API y gestionar el estado de loadScreen
-        setLoadScreen(true);
-    };
 
-    return (
-        <div className='abmLoggingScreen'>
-            <div className='abmLoggingScreen-content'>
-                <h1>¡Bienvenido a ABM FIT!</h1>
-                <p>¿Ya tienes cuenta con nosotros? Inicia sesión con tus datos.</p>
-                <button onClick={handleLoginClick}>Iniciar Sesión</button>
-                <p>¿No tienes cuenta? Regístrate aquí.</p>
-                <AddUserForm />
-            </div>
-        </div>
-    );
+  const handleLoginClick = () => {
+    setOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+
+
+
+  const sendDataUser = async (event) => {
+    event.preventDefault();
+
+    try {
+      const apiUrl = process.env.REACT_APP_API_URL || 'https://backendabmprojects.vercel.app';
+
+      // Endpoint para crear usuario
+      const createUserResponse = await axios.post(
+        `${apiUrl}/api/users/create`,
+        { email, password },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'X-Custom-Header': 'valor-personalizado'
+          }
+        }
+      );
+
+      // Manejar la respuesta del servidor
+      if (createUserResponse.status === 201) {
+        setMessage('Usuario añadido correctamente');
+        // Aquí podrías realizar más acciones, como limpiar los campos del formulario
+        setEmail('');
+        setPassword('')
+      } else {
+        setMessage('Error al añadir usuario');
+      }
+
+      // Ejemplo: Obtener todos los usuarios después de crear uno
+      const getUsersResponse = await axios.get(`${apiUrl}/api/users`);
+      console.log('Respuesta de obtener usuarios:', getUsersResponse.data);
+
+    } catch (error) {
+      console.error('Error al enviar el formulario:', error);
+      console.error('Respuesta del servidor:', error.response);
+      setMessage('Error al enviar el formulario');
+    }
+  };
+
+  return (
+    <div className="abmLoggingScreen">
+      <div className="abmLoggingScreen-content">
+        <h1>¡Bienvenido a ABM FIT!</h1>
+        <p>¿Ya tienes cuenta con nosotros? Inicia sesión con tus datos.</p>
+        <button onClick={handleLoginClick}>Iniciar Sesión</button>
+        <p>¿No tienes cuenta? Regístrate aquí.</p>
+        <AddUserForm />
+      </div>
+
+      <Modal show={openModal} onHide={handleCloseModal} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <form>
+        <input
+          type="email"
+          placeholder="Correo electrónico"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <p>{message}</p> {/* Mostrar mensaje al usuario */}
+      </form>        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Cerrar
+          </Button>
+          <Button variant="primary" onSubmit={sendDataUser}>Iniciar Sesión</Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
 };
-
 
 export default LoginUserScreen;
