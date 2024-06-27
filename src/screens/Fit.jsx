dad00509c9935e38eda01e5ef02e19f20d2af5d7
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-06-27 15:26:20 +0200

    fix

--- src/screens/Fit.jsx
+++ src/screens/Fit.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import CalendarioPrincipal from '../helpers/Calendar';
 import '../styles/Fit.css';
+import LoginUserScreen from './LoguinUserScreen';
 
 const Fit = () => {
   return (
     <div className='abmfitprincipal'>
       <h1>¡Bienvenido a ABM FIT!</h1>
       <CalendarioPrincipal />
+      <LoginUserScreen/>
     </div>
   );
 }
