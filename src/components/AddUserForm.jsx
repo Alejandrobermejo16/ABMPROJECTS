6cce6828a62f3f84cb007b6cd1ac5927ac6c9eee
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-07-17 10:11:36 +0200

    cambios en css padding para el boton

--- src/components/AddUserForm.jsx
+++ src/components/AddUserForm.jsx
@@ -1,13 +1,12 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import Spinner from 'react-bootstrap/Spinner';
-
+import React, { useState } from "react";
+import axios from "axios";
+import Spinner from "react-bootstrap/Spinner";
 
 const AddUserForm = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [message, setMessage] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(false); // Estado para el loader
 
   const handleSubmit = async (event) => {
@@ -15,7 +14,9 @@ const AddUserForm = () => {
     setLoading(true); // Mostrar loader al iniciar la petición
 
     try {
-      const apiUrl = process.env.REACT_APP_API_URL || 'https://backendabmprojects.vercel.app';
+      const apiUrl =
+        process.env.REACT_APP_API_URL ||
+        "https://backendabmprojects.vercel.app";
 
       // Endpoint para crear usuario
       const createUserResponse = await axios.post(
@@ -23,30 +24,29 @@ const AddUserForm = () => {
         { name, email, password }, // Envía name, email y password
         {
           headers: {
-            'Content-Type': 'application/json',
-            'X-Custom-Header': 'valor-personalizado'
-          }
+            "Content-Type": "application/json",
+            "X-Custom-Header": "valor-personalizado",
+          },
         }
       );
 
       // Manejar la respuesta del servidor
       if (createUserResponse.status === 201) {
-        setMessage('Usuario añadido correctamente');
-        setName('');
-        setEmail('');
-        setPassword('');
+        setMessage("Usuario añadido correctamente");
+        setName("");
+        setEmail("");
+        setPassword("");
       } else {
-        setMessage('Error al añadir usuario');
+        setMessage("Error al añadir usuario");
       }
 
       // Ejemplo: Obtener todos los usuarios después de crear uno
       const getUsersResponse = await axios.get(`${apiUrl}/api/users`);
-      console.log('Respuesta de obtener usuarios:', getUsersResponse.data);
-
+      console.log("Respuesta de obtener usuarios:", getUsersResponse.data);
     } catch (error) {
-      console.error('Error al enviar el formulario:', error);
-      console.error('Respuesta del servidor:', error.response);
-      setMessage('Error al enviar el formulario');
+      console.error("Error al enviar el formulario:", error);
+      console.error("Respuesta del servidor:", error.response);
+      setMessage("Error al enviar el formulario");
     } finally {
       setLoading(false); // Ocultar loader al finalizar la petición
     }
@@ -73,12 +73,18 @@ const AddUserForm = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button type="submit">Añadir Usuario</button>
+        <div style={{ paddingTop: "10px" }}>
+          
+          <button type="submit">Añadir Usuario</button>
+        </div>
         <p>{message}</p> {/* Mostrar mensaje al usuario */}
       </form>
-      {loading && <Spinner animation="border" role="status">
-      <span className="visually-hidden">Loading...</span>
-    </Spinner>} {/* Mostrar loader */}
+      {loading && (
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      )}
+      {/* Mostrar loader */}
     </div>
   );
 };
