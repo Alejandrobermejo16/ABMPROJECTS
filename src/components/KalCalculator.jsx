867df0d0b69da77093ee8db6bf561c031687c56c
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-07-19 14:56:31 +0200

    cambio de relleno de codigo

--- src/components/KalCalculator.jsx
+++ src/components/KalCalculator.jsx
@@ -30,8 +30,6 @@ function KalCalculator(props) {
     // Obtener correo electrónico desde sessionStorage
     const userEmail = sessionStorage.getItem('userEmail');
     if (userEmail) {
-      console.log('Correo electrónico almacenado en sessionStorage:', userEmail);
-      // Puedes usar userEmail como sea necesario en este componente
     } else {
       console.log('No hay correo electrónico almacenado en sessionStorage');
     }
