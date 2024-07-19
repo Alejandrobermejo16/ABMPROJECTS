4b34b68bc3be80f621f23cb61c78cd5dcb8eb75d
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-07-19 13:05:11 +0200

    prueba endpoint

--- src/helpers/Calendar.jsx
+++ src/helpers/Calendar.jsx
@@ -81,32 +81,48 @@ class CalendarioPrincipal extends Component {
     }, this.saveCalories); // Utilizamos una callback para guardar las calorías después de actualizar el estado
   };
 
+  checkCaloriesExists = async (userEmail) => {
+    try {
+      const apiUrl = process.env.REACT_APP_API_URL || "https://backendabmprojects.vercel.app";
+      const response = await axios.get(`${apiUrl}/api/users/cal`, {
+        params: { userEmail: userEmail }
+      });
+
+      return response.status === 200 && response.data.calories.length > 0;
+    } catch (error) {
+      console.error("Error al verificar la existencia de las calorías:", error);
+      return false;
+    }
+  };
+
   saveCalories = async () => {
     try {
       const apiUrl = process.env.REACT_APP_API_URL || "https://backendabmprojects.vercel.app";
       const userEmail = sessionStorage.getItem('userEmail'); // Obtener el correo electrónico del usuario desde sessionStorage
 
-      // Verificar URL y Payload
-      console.log("API URL:", `${apiUrl}/api/users/cal`);
-      console.log("Payload:", {
+      // Verificar si las calorías ya existen
+      const exists = await this.checkCaloriesExists(userEmail);
+
+      // Preparar los datos
+      const payload = {
         userEmail: userEmail,
         calories: this.state.cal
-      });
+      };
 
-      const response = await axios.post(
-        `${apiUrl}/api/users/cal`,
-        {
-          userEmail: userEmail, 
-          calories: this.state.cal
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
+      // Elegir el método HTTP basado en la existencia del campo
+      const response = exists
+        ? await axios.put(`${apiUrl}/api/users/cal`, payload, {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          })
+        : await axios.post(`${apiUrl}/api/users/cal`, payload, {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
 
-      if (response.status === 201) {
+      if (response.status === 200 || response.status === 201) {
         console.log("Datos de calorías guardados correctamente");
       } else {
         console.error("Error al guardar los datos de calorías");
