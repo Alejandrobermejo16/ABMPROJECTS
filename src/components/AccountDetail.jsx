5b82f426babc102c99161a554cd2d0031357e898
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-10-08 16:16:25 +0200

    nueva prueba

--- src/components/AccountDetail.jsx
+++ src/components/AccountDetail.jsx
@@ -5,20 +5,13 @@ import { useParams, useLocation } from "react-router-dom";
 const AccountDetail = () => {
   const { id } = useParams(); 
   const location = useLocation();
-  const { userData } = location.state || {}; // Accede a userData si está disponible
-
-
-  useEffect(() => {
-    // Log para verificar el valor de `id`
-    console.log("ID de la cuenta:", id);
-    // Log para verificar el contenido de `userData`
-    console.log("Datos del usuario:", userData);
-  }, [id, userData]); // Dependencias para ejecutar el efecto cuando cambien
+  const { userData } = location.state || {};
 
   return (
     <div className="container-Account">
       <h1>Detalle de la Cuenta</h1>
       <p>Número de cuenta: {id}</p>
+      <p>Nombre del Titular: {userData.data ? userData.data.name : 'Cargando...'}</p>
       <p>Nombre del Titular: {userData ? userData.name : 'Cargando...'}</p>
       <p>Límite de Crédito: {/* Lógica para mostrar límite de crédito */}</p>
       <p>Saldo Actual: {/* Lógica para mostrar saldo */}</p>
