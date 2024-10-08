d22409ada704d3d5720ea181df1473c7074a3ec1
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-10-08 14:48:45 +0200

    estructura de los detalles

--- src/components/AccountDetail.jsx
+++ src/components/AccountDetail.jsx
@@ -5,9 +5,13 @@ const AccountDetail = () => {
   const { id } = useParams(); // Obtener el id de la URL
 
   return (
-    <div>
+    <div className="container-Account">
       <h1>Detalle de la Cuenta</h1>
-      <p>ID de la cuenta: {id}</p>
+      <p>Numero de cuenta: {id}</p>
+      <p>Nombre del Titular:</p>
+      <p>Límite de Crédito:</p>
+      <p>Saldo Actual:</p>
+      <p>Estado de la Cuenta</p>
       {/* Aquí puedes hacer un fetch para obtener más detalles sobre la cuenta usando el ID */}
     </div>
   );
--- src/components/CardDetail.jsx
+++ src/components/CardDetail.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 
 function CardDetail() {
+
+    const { id } = useParams(); 
+
+
     return (
-        <div>
+        <div className="container-Card">
         <h1>Hello detalle de tarjeta</h1>
+        <p>Numero de Tarjeta:</p>
+        <p>Nombre del titular:</p>
+        <p>Fecha de Vencimiento</p>
+        <p>CVV inventar boton para verlo</p>
+        <p>Tipo de Tarjeta VISA MASTERCARD ETC</p>
+        <p>Límite de Credito</p>
+        <p>Gasto Actual</p>
         </div>
     );
 }
