beefc4986b1d31d1daf4406631f9a3b5b51a7056
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-10-09 13:56:19 +0200

    prueba

--- src/components/CardDetail.jsx
+++ src/components/CardDetail.jsx
@@ -4,25 +4,32 @@ import { useParams, useLocation } from "react-router-dom";
 function CardDetail() {
   const { id } = useParams(); // se obtiene de los parámetros de la url
   const location = useLocation();
-  const { userData } = location.state || {}; // Accede a cardData si está disponible
+  const { cardData } = location.state || {}; // Accede a cardData si está disponible
 
   // useEffect para hacer console.log de cardData al montar el componente
   useEffect(() => {
-    console.log("Card Data:", userData);
-  }, [userData]); // Se ejecutará cada vez que cardData cambie
+    console.log("Card Data:", cardData);
+  }, [cardData]); // Se ejecutará cada vez que cardData cambie
+
+
+  const card = cardData ? cardData[index] : null;
+
 
   return (
     <div className="container-Card">
-      {/* <h1>Detalle de Tarjeta</h1>
+      <h1>Detalle de Tarjeta</h1>
       <p>Número de Tarjeta: {id}</p>
-      <p>Nombre del titular: {cardData ? cardData.name : 'Cargando...'}</p>
-      <p>Fecha de Vencimiento: {cardData ? cardData.fech_caducidad : 'Cargando...'}</p>
-      <p>CVV: {cardData ? cardData.CVV : 'Cargando...'}</p>
-      <p>Tipo de Tarjeta: {cardData ? cardData.typeCard : 'Cargando...'}</p>
-      <p>Límite de Crédito: {cardData ? cardData.lim_credito : 'Cargando...'}</p> */}
+      <p>Nombre del titular: {card ? card.titular : 'Cargando...'}</p>
+      <p>Fecha de Vencimiento: {card ? card.fech_caducidad : 'Cargando...'}</p>
+      <p>CVV: {card ? card.CVV : 'Cargando...'}</p>
+      <p>Tipo de Tarjeta: {card ? card.typeCard : 'Cargando...'}</p>
+      <p>Límite de Crédito: {card ? card.lim_credito : 'Cargando...'}</p>
     </div>
   );
 }
 
 export default CardDetail;
+
+
+
 
