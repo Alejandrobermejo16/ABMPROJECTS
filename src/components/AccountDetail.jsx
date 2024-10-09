cadcd171f92d2ed5d3fe0d78c61b69b0dbac059c
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-10-09 13:41:01 +0200

    pruebas mostrando ya datos con la nueva correcion solo en cuentas

--- src/components/AccountDetail.jsx
+++ src/components/AccountDetail.jsx
@@ -17,10 +17,10 @@ const AccountDetail = () => {
     <div className="container-Account">
       <h1>Detalle de la Cuenta</h1>
       <p>Número de cuenta: {id}</p>
-      {/* <p>Nombre del Titular: {AccountData ? AccountData.name : 'Cargando...'}</p> */}
-      <p>Límite de Crédito: {/* Lógica para mostrar límite de crédito */}</p>
-      <p>Saldo Actual: {/* Lógica para mostrar saldo */}</p>
-      <p>Estado de la Cuenta: {/* Lógica para mostrar estado */}</p>
+       <p>Nombre del Titular: {AccountData ? AccountData.titular : 'Cargando...'}</p>
+      <p>Límite de Crédito: {AccountData ? AccountData.lim_credito : 'Cargando...'}</p>
+      <p>Saldo Actual: {AccountData ? AccountData.Saldo_Actual : 'Cargando...'}</p>
+      <p>Estado de la Cuenta: {AccountData ? AccountData.Estado_Cuenta : 'Cargando...'}</p>
     </div>
   );
 };
