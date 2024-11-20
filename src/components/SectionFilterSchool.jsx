e2a295cd5858aee671edcc9665c2a3266954928b
Alejandro <alejandrobermejomendez170712@gmail.com>
2024-11-20 11:41:49 +0100

    add:SectionFilterSchool component

---
+++ src/components/SectionFilterSchool.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/SectionFilterSchool.css';
+const SectionFilterSchool = () => {
+  return (
+    <div className='sectionPanel'>
+      <div className='menu'>
+        
+      </div>
+    </div>
+  );
+};
+
+export default SectionFilterSchool;
--- src/screens/School.jsx
+++ src/screens/School.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-import 'abm-components/src/components/abmmodal/abmmodal';
+import '../styles/School.css';
+import 'abm-components/src/page-es/abm-glass-filter/src/abm-glass-filter';
+import SectionFilterSchool from '../components/SectionFilterSchool';
 
 class School extends React.Component {
   render() {
     return (
-      <div style={{ backgroundColor: "blue", width: '100%' }}>
-        <h1>Welcome to the School</h1>
-        
-
+      <div className="div-scrolling">
+        <header className="header" style={{ backgroundColor: "blue", width: '100%' }}>
+          <h1 className="h1header">ABM SCHOOL</h1>
+          <abm-glass-filter searchValue="prueba"></abm-glass-filter>
+        </header>
+        <SectionFilterSchool />
       </div>
     );
   }
