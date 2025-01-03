73cfb1cca3d95fe7158f3efb72d3fa9975815511
Alejandro <alejandrobermejomendez170712@gmail.com>
2025-01-03 16:06:41 +0100

    changes: in css for mobile

--- src/components/NewInitialDashboard/NewInitialDashboardTechnologies.jsx
+++ src/components/NewInitialDashboard/NewInitialDashboardTechnologies.jsx
@@ -1,38 +1,36 @@
 import React from "react";
 import '../../styles/NewInitialDashboardTechnologies.css';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Cell } from "recharts";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Cell, ResponsiveContainer } from "recharts";
 import { LENGUAJESDEPROGRAMACION, FRAMEWORKS, OTROS, LIBRERIAS } from "../../Constants";
-
 
 // Componente reutilizable para los gráficos
 const CustomBarChart = ({ data, dataKey, colorKey }) => (
-  <BarChart  width={600} height={300} data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-    <CartesianGrid strokeDasharray="3 3" />
-    <XAxis  dataKey="name" />
-    <YAxis />
-    <Tooltip />
-    <Legend />
-    <Bar dataKey={dataKey}>
-      {data.map((entry, index) => (
-        <Cell key={`cell-${index}`} fill={entry[colorKey]} />
-      ))}
-    </Bar>
-  </BarChart>
+  <ResponsiveContainer width="80%" height={300}> 
+    <BarChart data={data}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Bar dataKey={dataKey}>
+        {data.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={entry[colorKey]} />
+        ))}
+      </Bar>
+    </BarChart>
+  </ResponsiveContainer>
 );
 
 const NewInitialDashboardTechnologies = () => {
-
   return (
-    <div className="divSkills">
-        <div className="divTech">
-          <div className="title-container">
-            <h1>Tecnologías que Impulsan Mi Trabajo</h1>
-          </div>
-          <CustomBarChart data={LENGUAJESDEPROGRAMACION} dataKey="nivel_Lenguaje" colorKey="color" />
-          <CustomBarChart data={FRAMEWORKS} dataKey="nivel_FrameWork" colorKey="color" />
-          <CustomBarChart data={OTROS} dataKey="otros" colorKey="color" />
-          <CustomBarChart data={LIBRERIAS} dataKey="nivel_Libreria" colorKey="color" />
-        </div>
+    <div className="divTech">
+      <div className="title-container">
+        <h1>Tecnologías que Impulsan Mi Trabajo</h1>
+      </div>
+      <CustomBarChart data={LENGUAJESDEPROGRAMACION} dataKey="nivel_Lenguaje" colorKey="color" />
+      <CustomBarChart data={FRAMEWORKS} dataKey="nivel_FrameWork" colorKey="color" />
+      <CustomBarChart data={OTROS} dataKey="otros" colorKey="color" />
+      <CustomBarChart data={LIBRERIAS} dataKey="nivel_Libreria" colorKey="color" />
     </div>
   );
 };
